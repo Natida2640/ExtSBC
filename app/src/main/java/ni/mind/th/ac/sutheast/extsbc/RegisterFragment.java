742842ff180b6568f596a4eeac751f86f18ecef5
natida <natidaonsa399@gmail.com>
2019-01-04 19:20:43 +0700

    Update(new)

--- app/src/main/java/ni/mind/th/ac/sutheast/extsbc/RegisterFragment.java
+++ app/src/main/java/ni/mind/th/ac/sutheast/extsbc/RegisterFragment.java
@@ -2,10 +2,17 @@ package ni.mind.th.ac.sutheast.extsbc;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import java.util.zip.Inflater;
 
 
 /**
@@ -18,6 +25,50 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolber
+        createToolber();
+
+//        Create Division
+        createDivision();
+
+    }   //Main Method
+
+    private void createDivision() {
+
+        String[] strings = new String[]{"ชั่นปีที่1","ชั่นปีที่2","ชั่นปีที่3","ชั่นปีที่4",};
+
+        Spinner spinner = getView().findViewById(R.id.spinnerDivision);
+    }
+
+    private void createToolber() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_have_space));
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+
+        inflater.inflate(R.menu.menu_register,menu);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
