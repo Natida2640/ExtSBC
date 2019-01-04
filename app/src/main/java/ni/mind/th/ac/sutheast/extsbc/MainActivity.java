1b179bd7f55f1b54fe1b0c983976deb4676f8be2
natida <natidaonsa399@gmail.com>
2019-01-04 14:16:30 +0700

    Update

--- app/src/main/java/ni/mind/th/ac/sutheast/extsbc/MainActivity.java
+++ app/src/main/java/ni/mind/th/ac/sutheast/extsbc/MainActivity.java
@@ -10,14 +10,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-//        Add Fragment to Activity
+//        Add Fradment to actvity
         if (savedInstanceState == null) {
 
-            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment,new MainFragment()).commit();
-
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
         }
-    } //Main Method
+
+
+    }   //Main Method
 
 } //Main Class
