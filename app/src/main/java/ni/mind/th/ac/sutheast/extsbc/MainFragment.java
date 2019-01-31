3f1cd85f7ed758c0d49a91e8023f3349c6fec453
Malinee <mind4minute@gmail.com>
2019-01-31 13:17:23 +0700

    Update Menu1

--- app/src/main/java/ni/mind/th/ac/sutheast/extsbc/MainFragment.java
+++ app/src/main/java/ni/mind/th/ac/sutheast/extsbc/MainFragment.java
@@ -7,23 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
+Button login ;
 
 
-    public MainFragment() {
-        // Required empty public constructor
-    }
 
-    @Override
+        @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
 //        Register Controller
         TextView textView = getView().findViewById(R.id.textRegister);
         textView.  setOnClickListener(new View.OnClickListener() {
@@ -31,18 +30,18 @@ public class MainFragment extends Fragment {
             public void onClick(View v) {
 //                Replace Fragment
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment, new RegisterFragment()).addToBackStack(null).commit();
-
             }
+
         });
 
 
     }   //Main Method
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
+
 
 }   //Main Class
--- app/src/main/java/ni/mind/th/ac/sutheast/extsbc/MenuFragment.java
+++ app/src/main/java/ni/mind/th/ac/sutheast/extsbc/MenuFragment.java
@@ -1,6 +1,7 @@
 package ni.mind.th.ac.sutheast.extsbc;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -59,12 +61,14 @@ public class MenuFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
